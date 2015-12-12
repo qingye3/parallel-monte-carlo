fea4092f1ac147e907f14431b21a91181e8c25aa
qingye3 <qingye3@illinois.edu>
2015-12-12 11:38:15 -0600

    all but energy calculation

--- subsweep.cu
+++ subsweep.cu
@@ -41,9 +41,51 @@ __device__ void proposed_move(float * proposed_move, float * D_sh, int i, curand
     }
 }
 
-__device__ short accept_move(float * proposed_move)
-{
+__device__ short out_of_bound(float * proposed_move, int cell_x, int cell_y, int cell_z){
+    float x = (cell_x * w - L / 2) / 2;
+    float y = (cell_y * w - L / 2) / 2;
+    float z = (cell_z * w - L / 2) / 2;
+    if (((proposed_move[0] - x) > L / 2 )|| ((proposed_move[0] - x) < - L / 2)){
+        return false;
+    }
+    if (((proposed_move[0] - y) > L / 2 )|| ((proposed_move[0] - y) < - L / 2)){
+        return false;
+    }
+    if (((proposed_move[0] - z) > L / 2 )|| ((proposed_move[0] - z) < - L / 2)){
+        return false;
+    }
+    return true;
+}
+float calculate_energy_in_cell(float * D_sh, int i, float * position){
     //TODO: implement this
+}
+
+
+float calculate_old_energy(int cell_x, int cell_y, int cell_z, float * D_sh, float * disk, int i, int atom_counts, short int * n){
+    //TODO: implement this
+}
+
+float calculate_new_energy(float * proposed_move, int cell_x, int cell_y, int cell_z, float * D_sh, float * disk, int i, int atom_counts, short int * n){
+    //TODO: implement this
+}
+
+
+
+__device__ short accept_move(float * proposed_move, int cell_x, int cell_y, int cell_z, float * disk, float * D_sh, int i, curandState_t * randomState, int atom_counts, short int * n)
+{
+    if (out_of_bound(proposed_move)){
+        return false;
+    }
+    float old_energy = calculate_old_energy(cell_x, cell_y, cell_z, D_sh, disk, i, atom_counts, n);
+    float new_energy = calculate_new_energy(proposed_move, cell_x, cell_y, cell_z, D_sh, disk, i, atom_counts, n);
+    if (new_energy < old_energy){
+        return true;
+    }
+    if (curand_uniform(random_state) < __expf(-beta * (new_energy - old_energy)){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 __device__ void cpy_proposed_to_D_sh(float * D_sh, float * proposed_move, int i) {
@@ -52,7 +94,7 @@ __device__ void cpy_proposed_to_D_sh(float * D_sh, float * proposed_move, int i)
     }
 }
 
-__global__ void subsweep_kernel(float* r, float * disk, short int *n, short int * offset){
+__global__ void subsweep_kernel(float * disk, short int *n, short int * offset){
     int cell_x = 2*(blockIdx.x * blockDim.x + threadIdx.x) + off[0];
     int cell_y = 2*(blockIdx.y * blockDim.y + threadIdx.y) + off[1];
     int cell_z = 2*(blockIdx.z * blockDim.z + threadIdx.z) + off[2];
@@ -75,7 +117,7 @@ __global__ void subsweep_kernel(float* r, float * disk, short int *n, short int 
     float proposed_move[3];
     for (int s = 0; s < n_M; n++){
         propose_move(proposed_move, D_sh, i, localRandomState);
-        if accept_move(proposed_move){
+        if accept_move(proposed_move, cell_x, cell_y, cell_z, disk, D_sh, i, atom_counts){
             cpy_proposed_to_D_sh(D_sh, proposed_move, i);
         }
         i += 1;
